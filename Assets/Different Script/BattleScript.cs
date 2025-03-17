267707df154fc90ff5d542ca36ff8016d0805ed9
Biohazzard <166527137+Biohazzard929@users.noreply.github.com>
2025-03-17 07:23:30 -0400

    Update BattleScript.cs

--- Assets/Different Script/BattleScript.cs
+++ Assets/Different Script/BattleScript.cs
@@ -15,15 +15,68 @@ public class BattleScript : MonoBehaviour
     public int startingMana = 4, maxMana = 10;
     public int playerMana;
 
-    public int startingCardsAmount = 4;
+    public int startingCardsAmount = 5;
+    public int cardsToDrawPerTurn = 0;
+
+    public enum TurnOrder {playerActive, playerCardAttacks, enemyActive, enemyCardAttacks};
+    public TurnOrder currentPhase;
+    private int currentPlayerMaxMana;
+
+    public void AdvanceTurn()
+        {
+            currentPhase++;
+            if((int)currentPhase >= System.Enum.GetValues(typeof(TurnOrder)).Length)
+            {
+                currentPhase = 0;
+            }
+
+            switch(currentPhase)
+            {
+            case TurnOrder.playerActive:
+                Debug.Log("Player Active");
+                UIController.instance.endTurnButton.SetActive(true);
+
+                if(currentPlayerMaxMana < maxMana)
+                {
+                    currentPlayerMaxMana++;
+                }
+                UIController.instance.drawCardButton.SetActive(true);
+                FillPlayerMana();
+
+                DeckController.instance.DrawMultipleCards(cardsToDrawPerTurn);
+                    break;
+            case TurnOrder.playerCardAttacks:
+                Debug.Log("Player Card Attacks");
+            // AdvanceTurn();
+                CardPointController.instance.PlayerAttack();
+                break;
+            case TurnOrder.enemyActive:
+                Debug.Log("Enemy Active");
+                AdvanceTurn();
+                break;           
+            case TurnOrder.enemyCardAttacks:
+                Debug.Log("Enemy Card Attacks");
+                AdvanceTurn();
+                break;            
+            }
+        }
+    public void EndPlayerTurn()
+        {
+            UIController.instance.endTurnButton.SetActive(false);
+            UIController.instance.drawCardButton.SetActive(false);
+            AdvanceTurn();
+        }
+     public void FillPlayerMana()
+    {
+        playerMana =  currentPlayerMaxMana = startingMana;
+        UIController.instance.SetPlayerManaText(playerMana);
+    }
 
     // Start is called before the first frame update
     void Start()
-    {
-        playerMana = startingMana;
-        UIController.instance.SetPlayerManaText(playerMana);
-
-        DeckController.instance.drawMultipleCards(startingCardsAmount);
+    { 
+        currentPlayerMaxMana = startingMana;
+        FillPlayerMana();
     }
 
     // Update is called once per frame
